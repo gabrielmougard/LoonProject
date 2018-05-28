4821fcc3d27fd1479508260df471e7f5aa32ea7b
gabrielmougard <gabriel.mougard@gmail.com>
2018-05-28 19:43:13 +0200

    Update stop.py

--- stop.py
+++ stop.py
@@ -1,3 +1,9 @@
 import RPi.GPIO as GPIO
 
+GPIO.output(16,False) #S_DAT_POST()
+GPIO.output(18,False) #servo
+GPIO.output(20,False) #G_DAT()
+GPIO.output(21,False) #R_ALT()
+GPIO.output(26,False) #S_DAT()
+
 GPIO.cleanup()
